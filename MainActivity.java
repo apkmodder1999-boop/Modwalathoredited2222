3d00f627ac0e7e12debb57e483bcf5610ac58c75
apkmodder1999-boop <apkmodder1999@gmail.com>
2026-07-01 15:25:09 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends Activity {
     private WebView webView;
     private final String targetTelegram = "https://t.me/pw0mod";
     private final String homeUrl = "https://pwthor.live/study/batches/69897f0ad7c19b7b2f7cc35f";
-    private final long EXPIRY_TIME_MS = 1785571737000L;
+    private final long EXPIRY_TIME_MS = 1785578055000L;
 
     private Handler urlCheckHandler = new Handler();
     private Runnable urlCheckRunnable;
