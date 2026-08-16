e24c0371ac72e9c8a253c6324c89597572bfcd98
apkmodder1999-boop <apkmodder1999@gmail.com>
2026-08-16 17:48:47 +0530

    Update URL blocking condition in MainActivity

--- MainActivity.java
+++ MainActivity.java
@@ -228,7 +228,7 @@ public class MainActivity extends Activity {
         }
 
         // Strict blocking if url ends exactly with /study/batches or /study/batches/
-        if (urlLower.endsWith("/love") || urlLower.endsWith("/love/")) {
+        if (urlLower.endsWith("/study/batches") || urlLower.endsWith("/study/batches")) {
             try {
                 webView.stopLoading();
                 webView.loadUrl(homeUrl);
